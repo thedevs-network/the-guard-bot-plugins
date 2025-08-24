f74acfaaa946727117fcc09dec500748676c89d8
Muthu Kumar <muthukumar@thefeathers.in>
2025-08-24 20:55:10 +0530

    feat: use config instead of env

--- plugins/anti-channel-reply.ts
+++ plugins/anti-channel-reply.ts
@@ -7,6 +7,14 @@ import { Composer, Context } from "telegraf";
 import { message } from "telegraf/filters";
 import type { Message, User } from "telegraf/types";
 
+const config = require("../config.json") as { excludeLinks?: string[] | string | false };
+
+const excludeLinks = config.excludeLinks
+	? Array.isArray(config.excludeLinks)
+		? config.excludeLinks
+		: [config.excludeLinks]
+	: [];
+
 interface From extends User {
 	status: "member" | "admin" | "banned";
 }
@@ -18,13 +26,14 @@ interface TheGuardContext extends Context {
 
 const reply = message("external_reply");
 
-const WHITELISTED = process.env.WHITELISTED_CHANNELS?.split(",") ?? [];
-
 export = Composer.on<TheGuardContext, typeof reply>(reply, async (ctx, next) => {
+	if (config.excludeLinks === false || excludeLinks.includes("*")) return next();
+
 	const chat = ctx.message.external_reply.chat;
 	if (chat?.type !== "channel") return next();
 
-	if (chat.username) if (WHITELISTED.includes(chat.username ?? "")) return next();
+	if (chat.username) if (excludeLinks.includes(chat.username)) return next();
+	if (excludeLinks.includes(chat.id.toString())) return next();
 
 	await ctx.deleteMessage();
 	await ctx.ban({
